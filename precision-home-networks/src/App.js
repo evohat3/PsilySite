cb18e9cb20c883cc0243925e1c2f9779ed2503f1
evohat3 <118646438+evohat3@users.noreply.github.com>
2024-01-05 05:33:09 -0600

    add navbar and navigation

--- precision-home-networks/src/App.js
+++ precision-home-networks/src/App.js
@@ -1,9 +1,22 @@
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Services from './pages/Services'
+import Navbar  from './components/Navbar';
 function App() {
   return (
-    <div >
+    <BrowserRouter>
+      <Navbar />
 
-    </div>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="Contact" element={<Contact />} />
+        <Route path="About" element={<About />} />
+        <Route path="Services" element={<Services />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- precision-home-networks/src/pages/Home.js
+++ precision-home-networks/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "animate.css"
 
 export default function Home() {
   return (
