7aabbaa654e7eb372064fc4983bbac78f13d860b
evohat3 <118646438+evohat3@users.noreply.github.com>
2024-01-08 08:02:21 -0600

    update styles.css, footer, navbar, home.js

---
+++ precision-home-networks/src/components/Background.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Background() {
+  return (
+    <div>
+    <div className='bg-slate-200 h-screen ' style={{
+        backgroundImage: 'url("https://cdn.discordapp.com/attachments/777325953202257930/1193829670890774539/image3.png?ex=65ae2385&is=659bae85&hm=eb766ec4164e018e354f2a6d363e997c00a4183ff7f48696454ed1e69dce8e6a&")',
+        backgroundSize: 'cover', // or 'contain', depending on your preference
+        backgroundRepeat: 'repeat',
+        backgroundPosition: 'center center',
+      }}> </div>
+
+    </div>
+  )
+}
--- precision-home-networks/src/pages/Home.js
+++ precision-home-networks/src/pages/Home.js
@@ -1,34 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import "animate.css"
-import "../styles/font.css"
+import React from 'react'
+import "animate.css";
+import "../styles/font.css";
 
 export default function Home() {
-
- 
-
   return (
-    <div className=''>
-      
-<div className='text-center p-4  animate__animated animate__backInRight bg-white textfont'>Welcome To Precision Network Solutions!</div>
+    <div className="">
+      <div className="text-center p-4 bg-white textfont">
+        <div className='animate__animated animate__backInRight'>Welcome To Precision Network Solutions!</div>
+        
+      </div>
 
-<div className='text-zinc-200 text-center text-5xl m-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]'>
+<div className=' size-fit flex items-center justify-center w-full '>
 
-<div className='m-4 p-4 focus-in-contract sans-serif'>Redefining Home Connectivity</div>
-<div className='m-4 p-4 focus-in-contract sans-serif'>Where Precision Meets Performance</div>
-
-</div>
-
-<div className='grid grid-cols-1 gap-1 sm:grid-cols-1 md:grid-cols-3 text-center p-3 h-screen'> 
-
-<div className='bg-white p-8 m-1 bg-slate-200 rounded-lg shadow-lg h-full md:h-1/2 '> TEST 1</div>
-
-<div className='bg-white p-8 m-1 bg-slate-100 rounded-lg shadow-lg h-full md:h-1/2'> TEST 1</div>
-
-<div className='bg-white p-8 m-1 bg-slate-200 rounded-lg shadow-lg h-full md:h-1/2'> TEST 1</div>
+      <div className="text-zinc-200 text-center sm:text-sm md:text-md lg:text-2xl xl:text-3xl 2xl:text-4xl m-20 drop-shadow-[0_35px_35px_rgba(0,0,0,1)]">
+        <div className="m-4 p-4 focus-in-contract sans-serif">
+          Redefining Home Connectivity
+        </div>
+        <div className="m-4 p-4 focus-in-contract sans-serif">
+          Where Precision Meets Performance
+        </div>
+      </div>
 
 </div>
 
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-1 md:grid-cols-3 text-center p-3 bg-slate-400 ">
+      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+ <div className="md:flex">
+   <div className="md:flex-shrink-0">
+   </div>
+   <div className="p-8">
+     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
+     <a href="Services" className="block mt-1 text-lg leading-tight font-medium text-black hover:bg-slate-300">Services</a>
+     <p className="mt-2 text-gray-500">Description of the card contentDescription of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.</p>
+   </div>
+ </div>
+</div>
 
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+ <div className="md:flex">
+   <div className="md:flex-shrink-0">
+   </div>
+   <div className="p-8">
+     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
+     <a href="About" className="block mt-1 text-lg leading-tight font-medium text-black hover:bg-slate-300">About</a>
+     <p className="mt-2 text-gray-500">Description of the card content. Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.</p>
+   </div>
+ </div>
+</div>
+
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+ <div className="md:flex">
+   <div className="md:flex-shrink-0">
+   </div>
+   <div className="p-8">
+     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold"></div>
+     <a href="Contact" className="block mt-1 text-lg leading-tight font-medium text-black hover:bg-slate-300">Contact US</a>
+     <p className="mt-2 text-gray-500">Description of the card contentDescription of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.Description of the card content.</p>
+   </div>
+ </div>
+</div>
+
+
+      </div>
     </div>
-  )
+  );
 }
