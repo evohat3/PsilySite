a6fd0dd40d97fd16dfc781586e8e108c406482ed
evohat3 <118646438+evohat3@users.noreply.github.com>
2024-01-13 09:16:16 -0600

    update server.js to serve path to build directory

--- precision-home-networks/Server/Server.js
+++ precision-home-networks/Server/Server.js
@@ -8,6 +8,18 @@ app.use(cors());
 app.use(cors({
   origin: 'https://precision-home-networks-a600700bbe20.herokuapp.com/'
 }));
+const path = require('path');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+
 
 const workEmail = process.env.Email;
 const webSiteEMail = process.env.User;
